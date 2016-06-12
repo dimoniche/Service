ec1b79fe35090dc3cabef97d9c3c0c5dbe03ca1d
Дмитрий Чистяков <dimoniche@live.com>
2016-06-12 14:26:56 +0300

    переименуем все

--- ServiceSaleMachine.Client/Properties/AssemblyInfo.cs
+++ ServiceSaleMachine.Client/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5cdb5513-96e8-4266-8b72-83679284b4e8")]
+[assembly: Guid("6a804bef-abb3-4a88-b230-f60967a71037")]
 
 // Version information for an assembly consists of the following four values:
 //

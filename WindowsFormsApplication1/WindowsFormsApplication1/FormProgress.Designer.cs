9b68553029a0edaf9edf293f4644a05008e5352a
Денис <cayden@ya.ru>
2016-06-22 18:09:11 +0300

    пробы интерфейса со всякими layout

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/FormProgress.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class FormProgress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(739, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(44, 491);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(65, 491);
+            this.panel2.TabIndex = 3;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(65, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.9646F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.0354F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 306F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(674, 491);
+            this.tableLayoutPanel1.TabIndex = 5;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.progressBar);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(3, 110);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(668, 71);
+            this.panel3.TabIndex = 7;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar.Location = new System.Drawing.Point(0, 0);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(668, 71);
+            this.progressBar.Step = 1;
+            this.progressBar.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(668, 107);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormProgress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 491);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormProgress";
+            this.Text = "Form2";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/SettingsData.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public class Service
+    {
+        public string caption { get; set; }
+        public string filename { get; set; }
+        public int timework { get; set; }
+
+
+    }
+
+
+
+//---------------------------------------------------------------------------------------------
+
+
+    public class SettingsData
+    {
+        List<Service> services;
+
+        public SettingsData()
+        {
+            services = new List<Service> { };
+        }
+
+        public int ServCount
+        {
+            get { return services.Count; }
+            set { }
+        }
+
+        public Service ServiceByIndex(int aIndex)
+        {
+            return services[aIndex]; 
+        }
+
+
+        public int LoadXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            string xmlname = "..\\services.xml";
+
+            if (System.IO.File.Exists(xmlname))
+            {
+                try
+                {
+                    doc.Load(xmlname);
+                }
+                catch
+                { return 1; }
+            }
+
+
+
+            XmlElement xRoot = doc.DocumentElement;
+            foreach (XmlNode node in xRoot)
+            {
+                Debug.Print(node.Name.ToString());
+                string filename = Environment.CurrentDirectory+ "\\..\\";
+                string caption = "";
+                int timework = 0;
+                foreach (XmlNode servnode in node.ChildNodes)
+                {
+                    if (servnode.Name == "image")
+                        filename = filename + servnode.InnerText;
+                    if (servnode.Name == "caption")
+                        caption = servnode.InnerText;
+                    if (servnode.Name == "timework")
+                        int.TryParse(servnode.InnerText, out timework);
+                    //                    Debug.Print(attr.Value.ToString());
+                }
+
+                if (System.IO.File.Exists(filename))
+                    {
+
+                        Service serv = new Service();
+                        serv.filename = filename;
+                        serv.caption = caption;
+                        serv.timework = timework;
+                        services.Add(serv);
+                    }
+            }
+            return 0;
+
+        }
+    }
+
+}
